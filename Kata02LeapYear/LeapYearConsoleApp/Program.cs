752d87eb9ba04c6a42562f4b4c7ed7a4feed1fa9
ahmed.rahman <ahmed.rahman@fdmgroup.com>
2024-04-25 17:40:22 +0100

    added logic to loop through year

--- Kata02LeapYear/LeapYearConsoleApp/Program.cs
+++ Kata02LeapYear/LeapYearConsoleApp/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using LeapYearConsoleApp.Businesslogic;
+using System;
+
+var leapYearobject = new LeapYear();
+
+try
+{
+    var currentYear = DateTime.Now.Year;
+
+    for (int year = 1; year <= currentYear; year++)
+    {
+        var isLeapYear = leapYearobject.IsLeapYear(year);
+        Console.WriteLine($"Year: {year},\nLeapYear: {isLeapYear}");
+    }
+    
+}
+catch (Exception exception)
+{
+    Console.WriteLine(exception.Message);
+}
+
+
