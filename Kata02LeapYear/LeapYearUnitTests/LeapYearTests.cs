311a75beaa6f0cdad366cf4099e3a6809ae00530
ahmed.rahman <ahmed.rahman@fdmgroup.com>
2024-04-25 17:10:52 +0100

    adding test checking if year is not a leap year not divisible by 4 this clears the a negative condition

--- Kata02LeapYear/LeapYearUnitTests/LeapYearTests.cs
+++ Kata02LeapYear/LeapYearUnitTests/LeapYearTests.cs
@@ -13,6 +13,21 @@ namespace LeapYearUnitTests
         }
 
         [Test]
+        [TestCase(2001, false)]
+        [TestCase(2007, false)]
+        [TestCase(2015, false)]
+        [TestCase(2021, false)]
+        [TestCase(2025, false)]
+        public void GivenTheCurrentYearIsNotLeapYear_WhenYearIsNotDivisbleByFour_ThenReturnFalse(int year, bool isLeapYearExpected)
+        {
+            //Act
+            var isLeapYearActual = leapYear.IsLeapYear(year);
+
+            //Assert
+            Assert.AreEqual(isLeapYearExpected, isLeapYearActual);
+        }
+
+        [Test]
         [TestCase(2004, true)]
         [TestCase(2008, true)]
         [TestCase(2012, true)]
